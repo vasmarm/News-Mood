0a6e0ec15799b7b7b7b4250e2c6f104c76e12e83
vasmarm <sharmavashisth@gmail.com>
2018-06-03 22:42:27 -0500

    Complete Assignment With Comments

---
+++ config.py
@@ -0,0 +1,6 @@
+#Twitter Key
+
+consumer_key = 'XYsmCYnSS9rlpqnbiCt2OqPsT'
+consumer_secret = 'mA002UKUmV7PD97o7uUVXNQg8ih2LpIyxEwxPtvoAcEmCiVobO'
+access_token = '114762424-5VjrMbF34IqPVZolqUEZjEoLUkuukgCpA7FmuFUZ'
+access_token_secret = 's3U5K9DdOx1uhJMysbdsFypC6qAeK42B5Ns4qh7nIiCqg'
